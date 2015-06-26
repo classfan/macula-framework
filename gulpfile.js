c149f88bfe482fd451502325f020b8ba8328b5df
Rain Wang <rainsoft@163.com>
2015-06-26 18:59:20 +0800

    Update 2015-06-26T10:59:18.780Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
-var gulp = require("gulp"),  
-  deploy = require("gulp-gh-pages");
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('publish', function () {  
-  gulp.src("_book/\*\*/\*.\*")
-    .pipe(deploy({
-      remoteUrl: "git@github.com:macula-projects/macula-framework.git"
-    }))
+  return gulp.src("_book/**/*")
+    .pipe(ghPages())
     .on("error", function(err){
       console.log(err)
     })
