aa0a2bb75145b3093ea68517c36636f163c79d8c
Rain Wang <rainsoft@163.com>
2015-06-26 18:14:41 +0800

    modify gulpfile.js

--- macula-docs/gulpfile.js
+++ macula-docs/gulpfile.js
@@ -2,10 +2,8 @@ var gulp = require("gulp"),
   deploy = require("gulp-gh-pages");
 
 gulp.task('publish', function () {  
-  gulp.src("_book/\*\*/\*.\*")
-    .pipe(deploy({
-      remoteUrl: "git@github.com:macula-projects/macula-framework.git"
-    }))
+  gulp.src("_book/**/*")
+    .pipe(deploy())
     .on("error", function(err){
       console.log(err)
     })
